dced6257d943581e5202ee1efbe5b23e7f46b9b7
AM <am@ampro.com>
2022-08-18 15:13:45 -0700

    rewrite and modularized the code

--- interest_rates_vs_payments.py
+++ interest_rates_vs_payments.py
@@ -11,6 +11,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def visualize_results(home_param):
+    '''
+    Plots the results including the range of interest rates on x-axis
+    monthly commitment on the y-axis for the different home prices.
+    the max budget line is also shown using dash-dot horizontal line.
+
+    Parameters
+    ----------
+    home_param: dict
+        parameters of the home and the calculated quantities
+
+    Returns
+    -------
+    None
+    '''
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+
+    # descriptive title for the figure
+    fig.suptitle(('Home value range: \\$%0.2fM - \\$%0.2fM, '
+                  'Down payment: \\$%dK, '
+                  '\nLoan Term: %d years, Monthly HOA/Mello-Roos: \\$%d, '
+                  'Monthly Maint.: \\$%d')
+                 % (home_param['home_val_lb']/1e6,
+                    home_param['home_val_ub']/1e6,
+                    home_param['down_pay']/1e3,
+                    home_param['loan_term'], home_param['hoa'],
+                    home_param['maint']))
+
+    # plot line series
+    # iterate through the different home values
+    for i in range(len(home_param['home_val'])):
+        ax.plot(np.squeeze(home_param['int_rate']),
+                home_param['mon_commit'][i, :],
+                label='%0.3f' % (home_param['home_val'][i]/1e6))
+
+    # add horizontal line showing the max budget
+    ax.axhline(home_param['mon_budget'],
+               linestyle='-.', linewidth=2, label='Max. budget')
+
+    ax.set_xlim([np.squeeze(home_param['int_rate'])[0],
+                 np.squeeze(home_param['int_rate'])[-1]])
+    ax.set_xlabel('Interest Rate [%]')
+    ax.set_ylabel('Monthly Commitment ($)')
+    ax.set_title('Monthly Commitment vs Interest Rate')
+    # plot legend box outside of the main figure area
+    ax.legend(title='Home price [\\$M]', bbox_to_anchor=(1.05, 1),
+              loc='upper left', borderaxespad=0.)
+
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
+
+    return
+
+
 def calc_mon_pay(out_prin, months, int_rate, loan_type):
     '''
     calculate monthly payment including interest and principal
@@ -56,6 +112,46 @@ def calc_mon_pay(out_prin, months, int_rate, loan_type):
     return [payment, interest, principal]
 
 
+def compute_mortgage_quantities(home_param):
+    '''
+    based on user inputs for the parameters of the home, and other purchase
+    factors such as HOA, property tax, etc., this function computes the
+    calculated mortgage quantities
+
+    Parameters
+    ----------
+    home_param: dict
+        dictionary that contains the main input quantities and also carries
+        the output and computed entities
+
+    Returns
+    -------
+    home_param: dict
+        dictionary that contains the main input quantities and also carries
+        the output and computed entities
+    '''
+
+    # calculate the schedule of payments
+    home_param['payment'], home_param['interest'], home_param['principal'] =\
+        calc_mon_pay(home_param['loan_amt'], home_param['loan_term']*12,
+                     home_param['int_rate'], home_param['loan_type'])
+
+    # monthly property tax
+    home_param['prop_tax'] = home_param['prop_tax_pct'] / (12*100) * \
+        home_param['home_val']
+
+    # monthly home insurance
+    home_param['home_ins'] = home_param['prop_tax_pct'] / (12*100) * \
+        home_param['home_val'] / 10
+
+    # total monthly commitment is sum of payment to bank, hoa, home ins and
+    # prop tax, maintenance
+    home_param['mon_commit'] = home_param['payment'] + home_param['hoa'] + \
+        home_param['home_ins'] + home_param['prop_tax'] + home_param['maint']
+
+    return home_param
+
+
 def get_valid_input(msg):
     '''
     gets valid inputs for the home prices or down payments in a variety of
@@ -97,6 +193,82 @@ def get_valid_input(msg):
     return val
 
 
+def get_inputs(home_param):
+    '''
+    gets user inputs for the parameters of the home, and other purchase
+    factors such as HOA, property tax, etc.
+
+    Parameters
+    ----------
+    home_param: dict
+        dictionary that contains the main input quantities and also carries
+        the output and computed entities
+
+    Returns
+    -------
+    home_param: dict
+        dictionary that contains the main input quantities and also carries
+        the output and computed entities
+    '''
+
+    # home value lower bound
+    home_param['home_val_lb'] = get_valid_input('Home price lower range: ')
+
+    # home value upper bound
+    home_param['home_val_ub'] = get_valid_input('Home price upper range: ')
+
+    # home price steps - like 25k, 50k, etc.
+    home_param['steps'] = get_valid_input('Home price steps: ')
+
+    # home value is in range between lower bound and upper bound
+    # in steps of user specified value.
+    home_param['home_val'] = \
+        np.array(range(int(home_param['home_val_lb']),
+                       int(home_param['home_val_ub']) + 1,
+                       int(home_param['steps'])))
+    # reshape to help with numpy broadcasting
+    home_param['home_val'] = home_param['home_val'].reshape(
+        len(home_param['home_val']), 1)
+
+    # downpayment - typically less than 30% of home value or amount
+    # enter maximum down payment possible
+    home_param['down_pay'] = \
+        get_valid_input('Enter max possible down payment: ')
+
+    # regular loan or interest only loan
+    home_param['loan_type'] = str(input('Reg. loan (R) or Int. only (I): '))
+
+    # loan amount is home value minus down payment
+    home_param['loan_amt'] = home_param['home_val'] - home_param['down_pay']
+
+    # loan term
+    home_param['loan_term'] = int(input('Loan term (years): '))
+
+    # Monthly HOA and Mello-Roos
+    home_param['hoa'] = int(input('Monthly HOA and Mello-Roos ($): '))
+
+    # Monthly maintenance
+    home_param['maint'] = int(input('Monthly maintenance ($): '))
+
+    # max monthly cash available
+    home_param['mon_budget'] = int(input('Monthly max budget ($): '))
+
+    # monthly property tax - typically around 1.25% in San Diego, CA area
+    home_param['prop_tax_pct'] = \
+        float(input('Enter property tax percentage (%): '))
+
+    # interest rate - simulate between desired interest rate range in
+    # steps of 0.25%
+    msg = 'Enter interest rate range seperated by (-): '
+    int_rate = np.array(input(msg).strip('% ').split('-'))
+    # adding delta of 0.01 to upper bound so that it is included
+    int_rate = np.arange(float(int_rate[0]), float(int_rate[-1])+0.01, 0.25)
+    # reshape to help with numpy broadcasting
+    home_param['int_rate'] = int_rate.reshape(1, len(int_rate))
+
+    return home_param
+
+
 def main():
     '''
     Calculates the monthly payments given mortgage parameters, and
@@ -111,101 +283,17 @@ def main():
     cash outflow and down payment.
     '''
 
-    # home value lower bound
-    home_val_lb = get_valid_input('Home price lower range: ')
+    # dictionary to store the essential parameters
+    home_param = {}
 
-    # home value upper bound
-    home_val_ub = get_valid_input('Home price upper range: ')
+    # get inputs
+    home_param = get_inputs(home_param)
 
-    # home price steps - like 25k, 50k, etc.
-    steps = get_valid_input('Home price steps: ')
+    # computing mortgage quantities
+    home_param = compute_mortgage_quantities(home_param)
 
-    # home value is in range between lower bound and upper bound
-    # in steps of user specified value.
-    home_val = np.array(range(int(home_val_lb), int(home_val_ub) + 1,
-                              int(steps)))
-    # reshape to help with numpy broadcasting
-    home_val = home_val.reshape(len(home_val), 1)
-
-    # downpayment - typically less than 30% of home value or amount
-    # enter maximum down payment possible
-    down_pay = get_valid_input('Enter max possible down payment: ')
-
-    # regular loan or interest only loan
-    loan_type = str(input('Reg. loan (R) or Int. only (I): '))
-
-    # loan amount is home value minus down payment
-    loan_amt = home_val - down_pay
-
-    # loan term
-    loan_term = int(input('Loan term (years): '))
-
-    # Monthly HOA and Mello-Roos
-    hoa = int(input('Monthly HOA and Mello-Roos ($): '))
-
-    # Monthly maintenance
-    maint = int(input('Monthly maintenance ($): '))
-
-    # max monthly cash available
-    mon_budget = int(input('Monthly max budget ($): '))
-
-    # monthly property tax - typically around 1.25% in San Diego, CA area
-    prop_tax_pct = float(input('Enter property tax percentage (%): '))
-
-    # interest rate - simulate between desired interest rate range in
-    # steps of 0.25%
-    msg = 'Enter interest rate range seperated by (-): '
-    int_rate = np.array(input(msg).strip('% ').split('-'))
-    # adding delta of 0.01 to upper bound so that it is included
-    int_rate = np.arange(float(int_rate[0]), float(int_rate[-1])+0.01, 0.25)
-    # reshape to help with numpy broadcasting
-    int_rate = int_rate.reshape(1, len(int_rate))
-
-    # calculate the schedule of payments
-    [payment, interest, principal] = \
-        calc_mon_pay(loan_amt, loan_term*12, int_rate, loan_type)
-
-    # monthly property tax
-    prop_tax = prop_tax_pct / (12*100) * home_val
-
-    # monthly home insurance
-    home_ins = prop_tax_pct / (12*100) * home_val / 10
-
-    # total monthly commitment is sum of payment to bank, hoa, home ins and
-    # prop tax, maintenance
-    mon_commit = payment + hoa + home_ins + prop_tax + maint
-
-    # plots the results including the range of interest rates on x-axis
-    # monthly commitment on the y-axis for the different home prices.
-    # the max budget line is also shown using dash-dot horizontal line
-
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-
-    # descriptive title for the figure
-    fig.suptitle(('Home value range: \\$%0.2fM - \\$%0.2fM, '
-                  'Down payment: \\$%dK, '
-                  '\nLoan Term: %d years, Monthly HOA/Mello-Roos: \\$%d, '
-                  'Monthly Maint.: \\$%d')
-                 % (home_val_lb/1e6, home_val_ub/1e6, down_pay/1e3,
-                    loan_term, hoa, maint))
-
-    # plot line series
-    # iterate through the different home values
-    for i in range(len(home_val)):
-        ax.plot(np.squeeze(int_rate), mon_commit[i, :],
-                label='%0.3f' % (home_val[i]/1e6))
-    # add horizontal line showing the max budget
-    ax.axhline(mon_budget, linestyle='-.', linewidth=2, label='Max. budget')
-    ax.set_xlim([np.squeeze(int_rate)[0], np.squeeze(int_rate)[-1]])
-    ax.set_xlabel('Interest Rate [%]')
-    ax.set_ylabel('Monthly Commitment ($)')
-    ax.set_title('Monthly Commitment vs Interest Rate')
-    # plot legend box outside of the main figure area
-    ax.legend(title='Home price [\\$M]', bbox_to_anchor=(1.05, 1),
-              loc='upper left', borderaxespad=0.)
-    plt.grid()
-    plt.tight_layout()
-    plt.show()
+    # visualize the results
+    visualize_results(home_param)
 
 
 if __name__ == '__main__':
